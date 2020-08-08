6ebe1522b6c1816292d266f2307299d21c834955
nguyenvandai61 <crystallifestyle05@gmail.com>
2020-08-08 22:44:40 +0700

    Generate random Korean, Chinese and Japanese words. Update Bot

--- src/main/java/com/example/apilanguage/utils/GenVocabulary.java
+++ src/main/java/com/example/apilanguage/utils/GenVocabulary.java
@@ -65,4 +65,51 @@ public class GenVocabulary {
         parentnode.put("data", arrayNode.toString());
         return parentnode;
     }
+
+    public static ObjectNode genVocabularyBot(String language, int num) throws IOException {
+        String link = "";
+        if (language.compareTo("english")==0) {
+            link = "https://www.bestrandoms.com/random-words";
+        }
+        else {
+            link = "https://www.bestrandoms.com/random-" + language + "-words";
+        }
+        System.out.println(link);
+        Connection.Response response2 = Jsoup.connect(link)
+                .method(Connection.Method.GET)
+                .execute();
+        Document responseDocument = response2.parse();
+        System.out.println(responseDocument);
+
+        // Change num of words
+        Element numWord = responseDocument.getElementsByClass("form-control").get(0);
+        numWord.attr("value", String.valueOf(num));
+        System.out.println(numWord);
+
+        // Submit form and gen words;
+        Element submitForm = responseDocument.getElementsByClass("form-horizontal").get(0);
+        FormElement form = (FormElement) submitForm;
+        Document newDoc = form.submit().post();
+        System.out.println(newDoc);
+        Elements list = newDoc.getElementsByClass("col-xs-12 col-sm-6");
+        System.out.println(list);
+
+        // Add word to your list and json them
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode parentnode = mapper.createObjectNode();
+        ArrayNode arrayNode = mapper.createArrayNode();
+        for (Element ele: list
+        ) {
+            Word word = splitWord(ele.toString());
+
+            ObjectNode childnode = mapper.createObjectNode();
+            childnode.put("text", word.getWord()+": "+word.getMeaning());
+            arrayNode.add(childnode);
+        }
+//
+//        // JSON wrapper
+        parentnode.put("message", arrayNode.toString());
+        return parentnode;
+    }
+
 }
